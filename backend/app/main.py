965bd64366234947c45519875d03721c1d61acfd
Nimesh Nilashan <nimesh.nilashan@gmail.com>
2026-08-10 22:37:37 +0530

    updated CORSMiddleware origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://argus-security-capstone.vercel.app/osint"],
+    allow_origins=["http://localhost:3000","https://argus-security-capstone.vercel.app/osint","https://argus-security-capstone.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
